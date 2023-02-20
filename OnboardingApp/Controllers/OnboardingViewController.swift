e13af67f2a879aeb97f4aa9e6990f0443fc932d0
msnbek <mahmutsenbek@icloud.com>
2023-02-20 21:26:02 +0300

    ui setup completed.

---
+++ OnboardingApp/Controllers/OnboardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingViewController.swift
+//  OnboardingApp
+//
+//  Created by Mahmut Senbek on 20.02.2023.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
